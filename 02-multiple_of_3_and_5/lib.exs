851379fb8d3f44a09efbc4d3b3fcaa6612e1379a
Phillip Gardner <pbg.gardner@gmail.com>
2021-04-12 15:16:13 +0100

    added third challenge

--- 02-multiple_of_3_and_5/lib.exs
+++ 02-multiple_of_3_and_5/lib.exs
@@ -8,16 +8,46 @@ end
 Challenge.solution(10)
 |> IO.inspect
 
+defmodule Kata do
+    def descending_order(n) do
+      n
+      |> Kernel.to_string()
+      |> String.split("", trim: true)
+      |> Enum.sort(:desc)
+      |> Enum.join
+      |> String.to_integer
+    end
+end
+
+Kata.descending_order(4235132)
+|> IO.inspect
+
+
 # defmodule Kata do
-#     def descending_order(n) do
-#       n
-#       |> Kernel.to_string()
-#       |> String.split("", trim: true)
-#       |> Enum.sort(:desc)
-#       |> Enum.join
-#       |> String.to_integer
+#     def capitalize(s) do
+#       oddstring = []
+#       evenstring = []
+#       s
+#       |> String.codepoints
+#       |> IO.inspect
+#       |> Enum.with_index
+#       |> Enum.each(fn({x, i}) ->
+#           cond do
+#             rem(i, 2) == 0 ->
+#               oddstring ++ [String.upcase(x)]
+#               |> IO.inspect
+#               evenstring ++ [String.downcase(x)]
+#             true ->
+#               evenstring ++ [String.upcase(x)]
+#               oddstring ++ [String.downcase(x)]
+#             end
+#       end)
+#       oddstring
+#       |> IO.inspect
+#       evenstring
+#       |> IO.inspect
 #     end
 # end
 
-# Kata.descending_order(4235132)
-# |> IO.inspect
+# Kata.capitalize("abcdef")
+# Kata.capitalize("codewars")
