731926eb60569b7cc04a67136857e069534b0a75
Phillip Gardner <pbg.gardner@gmail.com>
2021-04-12 19:18:19 +0100

    added mumbling challenge and wrote solution

--- mumbling/lib/mumbling.ex
+++ mumbling/lib/mumbling.ex
@@ -12,7 +12,19 @@ defmodule Mumbling do
       :world
 
   """
-  def hello do
-    :world
+  def accum(s) do
+    # your code
+    s
+    |> String.codepoints
+    |> Enum.with_index
+    |> Enum.map(fn {x, i} -> String.duplicate(x, i+1) |> String.capitalize end)
+    |> Enum.join("-")
   end
 end
+
+Mumbling.accum("ZpglnRxqenU")
+|> IO.inspect
+Mumbling.accum("NyffsGeyylB")
+|> IO.inspect
+Mumbling.accum("MjtkuBovqrU")
+|> IO.inspect
