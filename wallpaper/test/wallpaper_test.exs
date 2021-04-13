dd0169c5b41512c26b3218afdcacedd2c6c50106
Phillip Gardner <pbg.gardner@gmail.com>
2021-04-13 20:21:04 +0100

    created some testsSS

--- wallpaper/test/wallpaper_test.exs
+++ wallpaper/test/wallpaper_test.exs
@@ -5,4 +5,11 @@ defmodule WallpaperTest do
   test "greets the world" do
     assert Wallpaper.hello() == :world
   end
+
+  test "Basic Test" do
+    assert Wallpaper.wallpaper(6.3, 4.5, 3.29) == "sixteen"
+    assert Wallpaper.wallpaper(7.8, 2.9, 3.29) == "sixteen"
+    assert Wallpaper.wallpaper(6.3, 5.8, 3.13) == "seventeen"
+    assert Wallpaper.wallpaper(6.1, 6.7, 2.81) == "sixteen"
+  end
 end
