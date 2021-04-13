a08fe1d16fce385568c6a4dda9f2a7a77545508a
Phillip Gardner <pbg.gardner@gmail.com>
2021-04-13 20:09:57 +0100

    first commit

---
+++ wallpaper/lib/wallpaper.ex
@@ -0,0 +1,18 @@
+defmodule Wallpaper do
+  @moduledoc """
+  Documentation for `Wallpaper`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Wallpaper.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ wallpaper/test/wallpaper_test.exs
@@ -0,0 +1,8 @@
+defmodule WallpaperTest do
+  use ExUnit.Case
+  doctest Wallpaper
+
+  test "greets the world" do
+    assert Wallpaper.hello() == :world
+  end
+end
