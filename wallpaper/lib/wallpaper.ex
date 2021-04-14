8ce52ee41ce77c7efa1c1fe5b3e45084cb1ee18a
Phillip Gardner <pbg.gardner@gmail.com>
2021-04-14 12:53:37 +0100

    practicising another challenge

--- wallpaper/lib/wallpaper.ex
+++ wallpaper/lib/wallpaper.ex
@@ -1,8 +1,25 @@
 defmodule Wallpaper do
-
+  require Integer
   def wallpaper(l, w, h) do
     numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve","thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"]
     rolls = Kernel.trunc(Float.ceil(((1.15 * (2*l*h + 2*w*h)) / 5.2), 0))
     Enum.at(numbers, rolls)
   end
+
+  # def solution(str) do
+  #   # Your code here
+  #   str
+  #   |> Kernel.to_charlist()
+  #   |> IO.inspect
+  #   |> Enum.chunk_every(2, 2, '_')
+  #   |> IO.inspect
+  #   |> Enum.map(fn x -> to_string(x) end)
+  #   |> IO.inspect
+  # end
 end
+
+# Wallpaper.solution("abcdef")
+# |> IO.inspect
+# Wallpaper.solution("abcdefg")
+# |> IO.inspect
+
