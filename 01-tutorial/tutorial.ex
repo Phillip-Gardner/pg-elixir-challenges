aec5bc83268d2215255d553c2dfc446e8ea6315f
Phillip Gardner <pbg.gardner@gmail.com>
2021-04-15 11:12:57 +0100

    added short program

--- 01-tutorial/tutorial.ex
+++ 01-tutorial/tutorial.ex
@@ -2,4 +2,20 @@ defmodule M do
   def hello do
     IO.puts "Hello World"
   end
+
+  def wordmatch(guess) do
+    case guess do
+      "bread" ->
+        IO.puts "Correct!"
+      _wrong ->
+        IO.puts "Wrong answer"
+    end
+  end
 end
+
+hints = "water, bread, flour"
+IO.puts "Hints: #{hints}"
+
+guess = IO.gets "Guess the word "
+guess = String.trim(guess)
+M.wordmatch(guess)
