001a45aa716e979d118c949becf05578cdb57597
Phillip Gardner <pbg.gardner@gmail.com>
2021-04-19 17:08:00 +0100

    updated challenge

--- interview_challenge/lib/interview_challenge.ex
+++ interview_challenge/lib/interview_challenge.ex
@@ -1,18 +1,46 @@
 defmodule InterviewChallenge do
-  @moduledoc """
-  Documentation for `InterviewChallenge`.
-  """
+  def sum(list) do
+    case list do
+      [] ->
+        0
 
-  @doc """
-  Hello world.
+      [x] ->
+        x
 
-  ## Examples
+      list ->
+        hd(list) + sum(tl(list))
+    end
+  end
 
-      iex> InterviewChallenge.hello()
-      :world
+  def fibonacci(n) do
+    case n do
+      0 ->
+        0
 
-  """
-  def hello do
-    :world
+      1 ->
+        1
+
+      n ->
+        fibonacci(n - 1) + fibonacci(n - 2)
+    end
+  end
+
+  def pairs([]) do
+    []
+  end
+
+  def pairs([_number]) do
+    []
+  end
+
+  def pairs(list) do
+    # iterate over each elem of the list
+    # return the elem and elem[+1]
+    # push that to a result array
+    list
+    |> Enum.with_index
+    |> Enum.map(fn {number, index} -> [number, Enum.at(list, index + 1)] end)
+    |> Enum.filter(fn [_, b] -> b != nil end)
+    # |> Enum.chunk_every(2, 1, :discard)
   end
 end
--- interview_challenge/test/interview_challenge_test.exs
+++ interview_challenge/test/interview_challenge_test.exs
@@ -1,8 +1,25 @@
 defmodule InterviewChallengeTest do
   use ExUnit.Case
-  doctest InterviewChallenge
 
-  test "greets the world" do
-    assert InterviewChallenge.hello() == :world
+  test "adds up the numbers" do
+    assert InterviewChallenge.sum([]) == 0
+    assert InterviewChallenge.sum([3]) == 3
+    assert InterviewChallenge.sum([1, 2]) == 3
+    assert InterviewChallenge.sum([1, 2, 3]) == 6
+  end
+
+  test "fibonacci numbers" do
+    assert InterviewChallenge.fibonacci(0) == 0
+    assert InterviewChallenge.fibonacci(1) == 1
+    assert InterviewChallenge.fibonacci(2) == 1
+    assert InterviewChallenge.fibonacci(3) == 2
+    assert InterviewChallenge.fibonacci(10) == 55
+  end
+
+  test "sequential pairs" do
+    assert InterviewChallenge.pairs([]) == []
+    assert InterviewChallenge.pairs([1]) == []
+    assert InterviewChallenge.pairs([1, 2, 3]) == [[1, 2], [2, 3]]
+    assert InterviewChallenge.pairs([1, 2, 3, 4]) == [[1, 2], [2, 3], [3, 4]]
   end
 end
